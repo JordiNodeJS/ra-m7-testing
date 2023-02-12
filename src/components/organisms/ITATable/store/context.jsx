89faaf0acfbfba5d9f61ef27ddf37cdcb149bab3
JORDI.NODEJS <jordi.nodejs@gmail.com>
2023-02-12 20:46:51 +0100

    refactor-context-exports

--- src/components/organisms/ITATable/store/context.jsx
+++ src/components/organisms/ITATable/store/context.jsx
@@ -1,14 +1,10 @@
 import { createContext, useMemo, useReducer } from 'react'
 import { initialState, tableReducer } from './reducer'
 
-const TableContext = createContext(initialState)
+export const TableContext = createContext(initialState)
 
-function TableProvider({ children }) {
+export default function TableProvider({ children }) {
   const [state, dispatch] = useReducer(tableReducer, initialState)
   const value = useMemo(() => ({ state, dispatch }), [state])
   return <TableContext.Provider value={value}>{children}</TableContext.Provider>
 }
-
-export { TableContext }
-
-export default TableProvider
